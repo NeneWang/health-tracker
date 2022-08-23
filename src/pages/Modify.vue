12caaa746be832da7a23df016006cc05a8bf8935
NelsonWang <wangnelson2@gmail.com>
2022-08-23 00:48:48 -0400

    modify | default

--- src/pages/Modify.vue
+++ src/pages/Modify.vue
@@ -26,15 +26,11 @@ export default {
       value: {
         "Daily Habits": {
           type: "TEXT",
-          value:
-             "5:45am - 6:15am   | Wake-up -> Brush up teeth -> Prepare -> Go to bus stop.\
-              6:15am - 8:00am  | Go To Office | Do Anki/Readings on your way.\
-              8 :05am -9:00am   | Udemy: Related to Job\
-              9:00am  - 5:45pm | Work\
-              5:48pm - 7:30pm | Travel\
-              7:45pm - 8:30pm | Take a Bath, Eat, and do ASVAB\
-              8:30pm - 9:30pm | Start Pomodoro Blocks (Obj:6) Algorithms while Working Out\
-              9:30pm - 10:30pm | Buffer + 5 Udemy (10pm) + 2 Features Personal Project (10:30pm)",
+          value: "Wake up and trainning",
+        },
+        "DAY - What day is this report for?": { type: "DATETIME" },
+        "BLOCKS - How much Time blocks were you able to complete?": {
+          type: "NUMBER",
         },
         "Systems Applied ✅": {
           type: "CHECKBOX",
