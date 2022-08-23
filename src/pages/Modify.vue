69cc2f902f4812007ff3e3a8dbd53a71e63e1df4
NelsonWang <wangnelson2@gmail.com>
2022-08-23 00:32:21 -0400

    modify | jsonvue

--- src/pages/Modify.vue
+++ src/pages/Modify.vue
@@ -1,19 +1,14 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="q-pa-md" style="max-width: 500px">
+  <q-page>
+    <div>
       Modify Habits
-      <q-input v-model="habitsSetting" filled type="textarea" />
+      <!-- <q-input v-model="habitsSetting" filled type="textarea" /> -->
       <vue-json-editor
         v-model="json"
-        :show-btns="true"
         :mode="'code'"
-        lang="zh"
+        :value="value"
         @json-change="onJsonChange"
-        @json-save="onJsonSave"
-        @has-error="onError"
       >
-
-      
       </vue-json-editor>
 
       <button type="button" @click="resetJson">reset</button>
@@ -25,34 +20,22 @@
 import { defineComponent, ref } from "vue";
 import vueJsonEditor from "vue-json-editor";
 
-export default defineComponent({
-  name: "modifyPage",
-  components: {
-    vueJsonEditor,
-  },
+export default {
   data() {
     return {
-      json: [{ name: "Dirk", age: 1234 }, { name: "Jason" }],
+      value: {
+        msg: "demo of jsoneditor",
+      }
     };
   },
-  setup() {
-    return {
-      habitsSetting: ref("{'sadasd': true}"),
-    };
+
+  components: {
+    vueJsonEditor,
   },
   methods: {
     onJsonChange(value) {
       console.log("value:", value);
     },
-    onJsonSave(value) {
-      console.log("value:", value);
-    },
-    onError(value) {
-      console.log("value:", value);
-    },
-    resetJson() {
-      this.json = [{ name: "Dirk", age: 1234 }, { name: "Jason" }];
-    },
   },
-});
+};
 </script>
