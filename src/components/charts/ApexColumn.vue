98847d57a211d51820b7a42862e9757781394971
NelsonWang <wangnelson2@gmail.com>
2022-08-24 23:12:19 -0400

    heatmap | added

--- src/components/charts/ApexColumn.vue
+++ src/components/charts/ApexColumn.vue
@@ -1,40 +1,49 @@
 <template>
-  <apexchart height="300" type="bar" :options="options" :series="series"></apexchart>
+  <apexchart
+    height="300"
+    type="heatmap"
+    :options="options"
+    :series="series"
+  ></apexchart>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { getCssVar } from 'quasar'
+import { defineComponent } from "vue";
+import { getCssVar } from "quasar";
 
 export default defineComponent({
-  name: 'ApexColumn',
-  data () {
+  name: "ApexColumn",
+  data() {
     return {
       options: {
         title: {
-          text: 'ApexColumn',
-          align: 'left'
+          text: "ApexColumn",
+          align: "left",
         },
         chart: {
-          id: 'apex-column'
+          id: "apex-column",
         },
-        colors: [getCssVar('primary'), getCssVar('secondary'), getCssVar('negative')],
+        colors: [
+          getCssVar("primary"),
+          getCssVar("secondary"),
+          getCssVar("negative"),
+        ],
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
         },
         plotOptions: {
           bar: {
             horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'rounded'
-          }
-        }
+            columnWidth: "55%",
+            endingShape: "rounded",
+          },
+        },
       },
-      series: [{
-        name: 'series-1',
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
-      }]
-    }
-  }
-})
+      series: [
+        { name: "Monday", data: [30, 40, 45, 50, 49, 60, 70] },
+        { name: "Tuesday", data: [30, 40, 45, 50, 49, 60, 70] },
+      ],
+    };
+  },
+});
 </script>
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,15 +1,58 @@
 <template>
-  <q-page class="flex flex-center"> Profile </q-page>
+  <!-- <q-page class="flex flex-center"> Profile </q-page> -->
+  <q-card>
+    <apex-column />
+  </q-card>
 </template>
 
 <script>
-import { defineComponent, defineAsyncComponent  } from "vue";
-import VueApexCharts from "vue3-apexcharts";
+import { defineComponent, defineAsyncComponent } from 'vue'
+
+const ApexBar = defineAsyncComponent(() =>
+  import('src/components/charts/ApexBar.vue')
+)
+
+const ApexColumn = defineAsyncComponent(() =>
+  import('components/charts/ApexColumn.vue')
+)
+
+const ApexLine = defineAsyncComponent(() =>
+  import('components/charts/ApexLine.vue')
+)
+
+const ApexDonut = defineAsyncComponent(() =>
+  import('components/charts/ApexDonut.vue')
+)
+
+const ApexCandlestick = defineAsyncComponent(() =>
+  import('components/charts/ApexCandlestick.vue')
+)
+
+const ApexArea = defineAsyncComponent(() =>
+  import('components/charts/ApexArea.vue')
+)
+
+const ApexBubble = defineAsyncComponent(() =>
+  import('components/charts/ApexBubble.vue')
+)
+const HeatMapChart = defineAsyncComponent(()=>{
+  import('components/charts/HeatMapChart.vue')
+})
 
 // const app = createApp(App);
 
 export default defineComponent({
   name: "profilePage",
-  
+  components: {
+    // HeatMap,
+    HeatMapChart,
+    ApexBar,
+    ApexLine,
+    ApexColumn,
+    ApexDonut,
+    ApexCandlestick,
+    ApexArea,
+    ApexBubble,
+  },
 });
 </script>
