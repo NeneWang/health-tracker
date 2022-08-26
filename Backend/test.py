c04a57e3cb102dee3349bb9c274d855803a1fce9
Jingshi Liu <jingshiliu07@gmail.com>
2022-08-26 17:12:51 -0400

    Update test.py

--- Backend/test.py
+++ Backend/test.py
@@ -8,21 +8,31 @@ login_args = {
     'user_id': 4,
     'name': 'Jingshi Liu',
     'email': 'jingshiliu@email.com',
-    'password': '123456'
+    'password': '12456'
 }
 
 print(requests.post(BASE_URL + 'user', json=login_args).json())
 
 create_log_args = {
-    'user_id': 2,
-    'date': datetime.datetime.now().strftime('%b %d %Y %H'),
+    'user_id': 4,
+    'date': datetime.datetime.now().strftime('%b %d %Y %H %m %S'),
     'data': 'I am data!'
 }
+print(create_log_args['date'])
 
-# print(requests.post(BASE_URL + 'log', json=create_log_args).json())
+print(requests.post(BASE_URL + 'log', json=create_log_args).json())
 
 get_logs_args = {
-    'user_id': 2,
+    'user_id': 4,
 }
-# print(requests.get(BASE_URL + 'log', json=get_logs_args).json())
+print(requests.get(BASE_URL + 'log', json=get_logs_args).json())
+
+
+setting_post_args = {
+    'user_id': 4,
+    'data': 'i am dateeee'
+}
+
+
+print(requests.get(BASE_URL + 'settings', json=setting_post_args).json())
 
